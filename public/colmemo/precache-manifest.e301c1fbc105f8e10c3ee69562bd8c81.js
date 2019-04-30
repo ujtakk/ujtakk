695ae7524f22fa60858dc444dface88f8ad935b1
Takayuki Ujiie <ujtakk@gmail.com>
2019-04-30 20:03:21 +0900

    Added colmemo

---
+++ public/colmemo/precache-manifest.e301c1fbc105f8e10c3ee69562bd8c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3bf3285aede5b1f33cec828e8d1272",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce3206f158f3563188be",
+    "url": "./static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "ee4c64224d49d5e1a478",
+    "url": "./static/js/2.e9c595c7.chunk.js"
+  },
+  {
+    "revision": "ce3206f158f3563188be",
+    "url": "./static/js/main.b40ebdac.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
